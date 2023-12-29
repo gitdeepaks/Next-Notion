90eafe98e3a8d61b382d3c7e552e4d7180c89b5a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-12-29 15:43:36 +0530

    init new project 🚀

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fonts = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image
+        src="/logo.svg"
+        alt="logo"
+        height="40"
+        width="40"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        alt="logo"
+        height="40"
+        width="40"
+        className="hidden dark:block"
+      />
+      <p className={cn("font-semibold", fonts.className)}>Notion</p>
+    </div>
+  );
+};
---
+++ hooks/use-scroll-top.tsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+export const useScrollTop = (threshhold = 10) => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > threshhold) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [threshhold]);
+  return isScrolled;
+};
