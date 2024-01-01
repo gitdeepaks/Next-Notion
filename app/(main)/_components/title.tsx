ff545ecc0f4be903c9d90259731599da1276cc96
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-01 20:24:06 +0530

    added navbar and rename document 🚀

--- app/(main)/_components/title.tsx
+++ app/(main)/_components/title.tsx
@@ -2,9 +2,78 @@
 
 import { Doc } from "@/convex/_generated/dataModel";
 
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { useRef, useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { update } from "@/convex/documents";
+import { Skeleton } from "@/components/ui/skeleton";
+
 interface TitleProps {
   initialData: Doc<"documents">;
 }
 export const Title = ({ initialData }: TitleProps) => {
-  return <div>Title</div>;
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [title, setTitle] = useState(initialData.title || "untitled");
+  const enableInput = () => {
+    setTitle(initialData.title);
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+    }, 0);
+  };
+
+  const disableInput = () => {
+    setIsEditing(false);
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+    update({
+      id: initialData._id,
+      title: e.target.value || "Untitleed",
+    });
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      disableInput();
+    }
+  };
+
+  const update = useMutation(api.documents.update);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  return (
+    <div className="flex items-center gap-x-1">
+      {!!initialData.icon && <p>{initialData.icon}</p>}
+      {isEditing ? (
+        <Input
+          ref={inputRef}
+          onClick={enableInput}
+          onBlur={disableInput}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+          value={title}
+          className="h-7 focus-visible:ring-transparent"
+        />
+      ) : (
+        <Button
+          onClick={enableInput}
+          variant="ghost"
+          size="sm"
+          className="font-normal h-auto p-1"
+        >
+          <span className="truncate">{initialData.title}</span>
+        </Button>
+      )}
+    </div>
+  );
+};
+
+Title.Skeleton = function TitleSkeleton() {
+  return <Skeleton className="h-9 w-16 rounded-md" />;
 };
