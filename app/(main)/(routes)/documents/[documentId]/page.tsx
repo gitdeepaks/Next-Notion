34124f16926aaf41688677b43f84482e98d3644e
gitdeepaks <deepaksankhyan92@outlook.com>
2024-01-02 22:19:52 +0530

    added toolbat partially 🚀

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
 
-const DocumentIdPage = () => {
-  return <div>DocumentId</div>;
+import React from "react";
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { ToolBar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+  if (document === undefined) {
+    return <div>Loading...</div>;
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-3xl lg:max-w-4xl mx-auto">
+        <ToolBar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
